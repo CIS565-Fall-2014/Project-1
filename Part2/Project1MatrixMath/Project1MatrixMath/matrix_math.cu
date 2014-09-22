fc65c2531e5140ee39498ddfa8db764c616cbf1a
unknown <mbli@MOR100C-02.base.seas.upenn.edu>
2014-09-21 21:42:26 -0400

    matrix_math.cu hello world is working

--- Part2/Project1MatrixMath/Project1MatrixMath/matrix_math.cu
+++ Part2/Project1MatrixMath/Project1MatrixMath/matrix_math.cu
@@ -1,10 +1,38 @@
+/* 
+ * MICHAEL B LI
+ * CIS 565 Project-1
+ * 
+ * "Hello world" made possible with support from http://www.brainlings.com/2011/11/hello-world-in-cuda/
+ * 
+ * 
+ */
+
 #include <iostream>
 using namespace std;
 
-int main() {
+
+
+
+
+
+int main(int argc, char** argv) {
 
 	cout << "Hello there!" << endl;
 
+	cin.ignore();
+
 	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
