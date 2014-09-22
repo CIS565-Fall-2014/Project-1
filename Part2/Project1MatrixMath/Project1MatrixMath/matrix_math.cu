de2e36c1462ff09a6b90ce6d6f5672a2e614dfa2
unknown <mbli@MOR100C-02.base.seas.upenn.edu>
2014-09-21 22:17:04 -0400

    nothing interesting here

--- Part2/Project1MatrixMath/Project1MatrixMath/matrix_math.cu
+++ Part2/Project1MatrixMath/Project1MatrixMath/matrix_math.cu
@@ -8,31 +8,43 @@
  */
 
 #include <iostream>
-using namespace std;
 
-
-
-
+#define MAT_WIDTH 5
 
 
 int main(int argc, char** argv) {
 
-	cout << "Hello there!" << endl;
+	//std::cout << "Hello there!" << std::endl;
 
-	cin.ignore();
+	//std::cin.ignore();
 
+	//return 0;
+
+	//#################################################################################
+
+
+	// CPU float array
+	// I will just be using a 1D array to hold the floats.
+	// MAT_WIDTH is defined to be 5 above; change it to test with something else. All matrices assumed to be SQUARE.
+
+	float* M = new float[MAT_WIDTH*MAT_WIDTH]();
+	for (int i = 0; i < MAT_WIDTH*MAT_WIDTH; i++) {
+		M[i] = float(i);
+	}
+
+	for (int i = 0; i < 26; i++) {
+		std::cout << M[i] << std::endl;
+	}
+
+	int size = MAT_WIDTH * MAT_WIDTH * sizeof(float);
+
+
+	// will be using global memory only (cudaMalloc), since using __shared__ was not specified in the directions
+	// and this project is already difficult enough such that I'd rather not give myself extra work.
+
+
+
+
+	std::cin.ignore();
 	return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
